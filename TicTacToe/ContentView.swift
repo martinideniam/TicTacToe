889a32aa4b10724dc043ccd336863d4a57ab5937
martinideniam <martinideniam@gmail.com>
2022-04-19 02:23:10 +0300

    TicTacToe - first commit

--- TicTacToe/ContentView.swift
+++ TicTacToe/ContentView.swift
@@ -8,9 +8,62 @@
 import SwiftUI
 
 struct ContentView: View {
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            Label()
+            Board()
+        }
+    }
+}
+
+struct Label: View {
+    var body: some View {
+        Text("Крестики Нолики")
+    }
+}
+
+struct Board: View {
+    var body: some View {
+        Cells()
+    }
+}
+
+struct Cells: View {
+    var body: some View {
+        VStack {
+            HStack {
+                Cell(state: "a1")
+                Cell(state: "a2")
+                Cell(state: "a3")
+            }
+            
+            HStack {
+                Cell(state: "b1")
+                Cell(state: "b2")
+                Cell(state: "b3")
+            }
+            HStack {
+                Cell(state: "c1")
+                Cell(state: "c2")
+                Cell(state: "c3")
+            }
+        }
+    }
+}
+
+struct Cell: View {
+    var state: String?
+
+    var body: some View {
+        ZStack {
+            RoundedRectangle(cornerRadius: 10)
+                .frame(width: 100, height: 100, alignment: .center)
+            if let someState = state {
+                Text(someState)
+                    .foregroundColor(.white)
+            }
+        }
     }
 }
 
