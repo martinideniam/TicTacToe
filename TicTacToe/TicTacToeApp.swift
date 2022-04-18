3ac5af684859d993ec5cbaa990c42815cec8efb0
martinideniam <martinideniam@gmail.com>
2022-04-19 01:52:11 +0300

    Initial Commit

---
+++ TicTacToe/TicTacToeApp.swift
@@ -0,0 +1,17 @@
+//
+//  TicTacToeApp.swift
+//  TicTacToe
+//
+//  Created by Vladislav Gorovenko on 19.04.2022.
+//
+
+import SwiftUI
+
+@main
+struct TicTacToeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
